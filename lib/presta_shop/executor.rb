cb071dcea1f919b280a50e247b871404e82d940c
benoit.beaudenon@gmail.com <benoit.beaudenon@gmail.com>
2015-05-04 12:49:55 +0200

    Request.method instead of RestClient::Request.execute for post

--- lib/presta_shop/executor.rb
+++ lib/presta_shop/executor.rb
@@ -10,10 +10,28 @@ module PrestaShop
         puts configuration.headers
         puts configuration.verify_ssl
         # Make an request
-        response = RestClient::Request.execute  :method   => options[:method], 
-                                                :url       => url.to_s,
-                                                :user       => configuration.api_key,
-                                                :headers  => configuration.headers
+
+        case options[:method]
+          when "post"
+            OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE unless configuration.verify_ssl
+            uri = URI.parse(url.to_s)
+            first_arg = uri.scheme + "://" + configuration.api_key + "@" + uri.host + uri.path
+            second_arg = CGI.parse(URI.parse(uri).query)["xml"][0]
+            response = RestClient.post first_arg, second_arg
+          else
+            response = RestClient::Request.execute  :method   => options[:method],
+                                                    :url       => url.to_s,
+                                                    :user       => configuration.api_key,
+                                                    :headers  => configuration.headers,
+                                                    :verify_ssl => configuration.verify_ssl
+        end
+
+        # response = RestClient::Request.execute  :method   => options[:method],
+        #                                         :url       => url.to_s,
+        #                                         :user       => configuration.api_key,
+        #                                         :headers  => configuration.headers,
+        #                                         :verify_ssl => configuration.verify_ssl
+
         puts response.inspect
         # Validate if PrestaShop version is supported
         Headers.new(response).validate!
