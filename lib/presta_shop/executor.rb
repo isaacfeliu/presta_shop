b8ecd6781e8fde97f0c2d63e27369b214bd438c3
benoit.beaudenon@gmail.com <benoit.beaudenon@gmail.com>
2015-05-05 20:42:01 +0200

    Cleanup

--- lib/presta_shop/executor.rb
+++ lib/presta_shop/executor.rb
@@ -3,30 +3,21 @@ module PrestaShop
         # Check if url and query params are valid
         url = URLResolver.new configuration, options
         url.validate!
-        puts url
-        puts options[:method]
-        puts url.to_s
-        puts configuration.api_key
-        puts configuration.headers
-        puts configuration.verify_ssl
         # Make an request
 
         case options[:method]
-          when :post
-            #ssl.verify_mode = OpenSSL::SSL::VERIFY_NONE unless configuration.verify_ssl
-            uri = URI.parse(url.to_s)
-            first_arg = uri.scheme + "://" + configuration.api_key + "@" + uri.host + uri.path
-            second_arg = CGI.parse(uri.query)["xml"][0]
-            puts "POST*******************"
-            puts first_arg
-            puts second_arg
-            response = RestClient.post first_arg, second_arg
-          else
+          when :get
             response = RestClient::Request.execute  :method   => options[:method],
                                                     :url       => url.to_s,
                                                     :user       => configuration.api_key,
                                                     :headers  => configuration.headers,
                                                     :verify_ssl => configuration.verify_ssl ? true : false
+          else
+            #ssl.verify_mode = OpenSSL::SSL::VERIFY_NONE unless configuration.verify_ssl
+            uri = URI.parse(url.to_s)
+            first_arg = uri.scheme + "://" + configuration.api_key + "@" + uri.host + uri.path
+            second_arg = CGI.parse(uri.query)["xml"][0]
+            response = RestClient.post first_arg, second_arg
         end
 
         # response = RestClient::Request.execute  :method   => options[:method],
